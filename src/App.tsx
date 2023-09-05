c3cd5547185ab03e13d1a8c069da7286afd72126
Karan <packirisamykaran@gmail.com>
2023-09-05 13:11:29 +0800

    Solana key pair

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import { Keypair } from '@solana/web3.js';
+import { generateMnemonic, mnemonicToSeed } from 'bip39';
+import { Buffer } from 'buffer';
+
+// Add the following line to polyfill Buffer
+(global as any).Buffer = Buffer;
+async function generateSolanaWallet(): Promise<{
+  publicKey: string;
+  secretKeyBase58: string;
+  secretKeyText: string;
+  recoveryPhrase: string;
+}> {
+  // Generate a BIP39 mnemonic (recovery phrase)
+  const mnemonic = generateMnemonic();
+
+  // Derive a Solana key pair from the mnemonic
+  const seed = await mnemonicToSeed(mnemonic);
+  const keyPair = Keypair.fromSeed(seed.slice(0, 32)); // Use the first 32 bytes as the seed
+  const secretKeyBuffer = Buffer.from(keyPair.secretKey);
+
+  return {
+    publicKey: keyPair.publicKey.toBase58(),
+    secretKeyBase58: keyPair.secretKey.toString(),
+    secretKeyText: secretKeyBuffer.toString('hex'), // Convert to plain text
+    recoveryPhrase: mnemonic,
+  };
+}
+
+// Example usage
+async function main() {
+  const {
+    publicKey,
+    secretKeyBase58,
+    secretKeyText,
+    recoveryPhrase,
+  } = await generateSolanaWallet();
+
+  console.log('Public Key:', publicKey);
+  console.log('Secret Key (Base58):', secretKeyBase58);
+  console.log('Secret Key (Text):', secretKeyText); // Display the secret key in plain text
+  console.log('Recovery Phrase:', recoveryPhrase);
+}
+
+main().catch((error) => console.error(error));
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
---
+++ src/custom.d.ts
@@ -0,0 +1,8 @@
+declare module "bsv" {
+  const Bip39: any;
+  const Bip32: any;
+  const Ed25519HDKey: any;
+  const CoinType: any;
+
+  export { Bip39, Bip32, Ed25519HDKey, CoinType };
+}
