7b27a2ccbd1d01cd9bf944c970f39a0c8c8f66ef
Karan <packirisamykaran@gmail.com>
2023-09-06 21:08:07 +0800

    Overflow fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,46 @@
-
-
 import './App.css';
 import { Keypair } from '@solana/web3.js';
 import { generateMnemonic, mnemonicToSeed } from 'bip39';
 import { Buffer } from 'buffer';
 import React, { useState } from 'react';
-import { Box, Button, Select, VStack, Text, useBreakpointValue } from '@chakra-ui/react';
+import { Box, Button, Select, Text, Spacer, VStack, useBreakpointValue } from '@chakra-ui/react';
 import { saveAs } from 'file-saver';
 
 
 
 
-
-// Add the following line to polyfill Buffer
-(global as any).Buffer = Buffer;
-async function generateSolanaWallet(): Promise<{
+// Define a type or interface for wallet data
+interface WalletData {
   publicKey: string;
   secretKeyBase58: string;
   secretKeyText: string;
   recoveryPhrase: string;
-}> {
-  // Generate a BIP39 mnemonic (recovery phrase)
-  const mnemonic = generateMnemonic();
-
-  // Derive a Solana key pair from the mnemonic
-  const seed = await mnemonicToSeed(mnemonic);
-  const keyPair = Keypair.fromSeed(seed.slice(0, 32)); // Use the first 32 bytes as the seed
-  const secretKeyBuffer = Buffer.from(keyPair.secretKey);
-
-  return {
-    publicKey: keyPair.publicKey.toBase58(),
-    secretKeyBase58: keyPair.secretKey.toString(),
-    secretKeyText: secretKeyBuffer.toString('hex'), // Convert to plain text
-    recoveryPhrase: mnemonic,
-  };
 }
 
-// Example usage
-async function main() {
 
-
-
-  const {
-    publicKey,
-    secretKeyBase58,
-    secretKeyText,
-    recoveryPhrase,
-  } = await generateSolanaWallet();
-
-  console.log('Public Key:', publicKey);
-  console.log('Secret Key (Base58):', secretKeyBase58);
-  console.log('Secret Key (Text):', secretKeyText); // Display the secret key in plain text
-  console.log('Recovery Phrase:', recoveryPhrase);
-}
-
-main().catch((error) => console.error(error));
+// Add the following line to polyfill Buffer
+(global as any).Buffer = Buffer;
 
 
 function App() {
-  const [selectedBlockchain, setSelectedBlockchain] = useState('Ethereum');
-  const [walletData, setWalletData] = useState(null);
+  const [selectedBlockchain, setSelectedBlockchain] = useState('Solana');
+  const [walletData, setWalletData] = useState<WalletData | null>(null);;
 
-  const generateWallet = () => {
+  const generateWallet = async () => {
+
+    // Generate a BIP39 mnemonic (recovery phrase)
+    const mnemonic = generateMnemonic();
+
+    // Derive a Solana key pair from the mnemonic
+    const seed = await mnemonicToSeed(mnemonic);
+    const keyPair = Keypair.fromSeed(seed.slice(0, 32)); // Use the first 32 bytes as the seed
+    const secretKeyBuffer = Buffer.from(keyPair.secretKey);
+
     // Generate wallet data here
-    const publicKey = 'your_public_key';
-    const secretKeyBase58 = 'your_secret_key_base58';
-    const secretKeyText = 'your_secret_key_text';
-    const recoveryPhrase = 'your_recovery_phrase';
+    let publicKey = keyPair.publicKey.toBase58();
+    let secretKeyBase58 = keyPair.secretKey.toString();
+    let secretKeyText = secretKeyBuffer.toString('hex'); // Convert to plain text
+    let recoveryPhrase = mnemonic;
 
     // Store wallet data in state
     setWalletData({ publicKey, secretKeyBase58, secretKeyText, recoveryPhrase });
@@ -108,12 +82,24 @@ function App() {
           Generate Wallet
         </Button>
         {walletData && (
-          <Box width="80%" maxWidth="300px" borderWidth="1px" p={4} borderRadius="md">
-            <Text fontWeight="bold">Wallet Data</Text>
-            <Text>Public Key: {walletData.publicKey}</Text>
-            <Text>Secret Key Base58: {walletData.secretKeyBase58}</Text>
-            <Text>Secret Key Text: {walletData.secretKeyText}</Text>
-            <Text>Recovery Phrase: {walletData.recoveryPhrase}</Text>
+          <Box width="80%" maxWidth="400px" borderWidth="1px" p={4} borderRadius="md">
+            <Text fontWeight="bold" fontSize="lg">Wallet Data</Text>
+            <Spacer h="1rem" />
+            <Text>
+              <b>Public Key:</b> <span style={{ maxWidth: '100%' }}>{walletData.publicKey}</span>
+            </Text>
+            <Spacer h="1rem" />
+            <Text>
+              <b>Secret Key Base58:</b> <span style={{ maxWidth: '100%' }}>{walletData.secretKeyBase58}</span>
+            </Text>
+            <Spacer h="1rem" />
+            <Text>
+              <b>Secret Key Text:</b> <span style={{ maxWidth: '100%' }}>{walletData.secretKeyText}</span>
+            </Text>
+            <Spacer h="1rem" />
+            <Text>
+              <b>Recovery Phrase:</b> <span style={{ maxWidth: '100%' }}>{walletData.recoveryPhrase}</span>
+            </Text>
             <Button
               size={buttonSize}
               colorScheme="blue"
