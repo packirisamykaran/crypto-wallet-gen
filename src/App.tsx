2524f2ef3523d8c52a57affeba37cc8fd086fc14
Karan <packirisamykaran@gmail.com>
2023-09-06 19:40:04 +0800

    Base layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+
 
 import './App.css';
 import { Keypair } from '@solana/web3.js';
 import { generateMnemonic, mnemonicToSeed } from 'bip39';
 import { Buffer } from 'buffer';
+import React, { useState } from 'react';
+import { Box, Button, Select, VStack, Text, useBreakpointValue } from '@chakra-ui/react';
+import { saveAs } from 'file-saver';
+
+
+
+
 
 // Add the following line to polyfill Buffer
 (global as any).Buffer = Buffer;
@@ -31,6 +38,9 @@ async function generateSolanaWallet(): Promise<{
 
 // Example usage
 async function main() {
+
+
+
   const {
     publicKey,
     secretKeyBase58,
@@ -48,9 +58,74 @@ main().catch((error) => console.error(error));
 
 
 function App() {
+  const [selectedBlockchain, setSelectedBlockchain] = useState('Ethereum');
+  const [walletData, setWalletData] = useState(null);
+
+  const generateWallet = () => {
+    // Generate wallet data here
+    const publicKey = 'your_public_key';
+    const secretKeyBase58 = 'your_secret_key_base58';
+    const secretKeyText = 'your_secret_key_text';
+    const recoveryPhrase = 'your_recovery_phrase';
+
+    // Store wallet data in state
+    setWalletData({ publicKey, secretKeyBase58, secretKeyText, recoveryPhrase });
+  };
+
+  const downloadAsJSON = () => {
+    if (walletData) {
+      const walletJSON = JSON.stringify(walletData, null, 2);
+      const blob = new Blob([walletJSON], { type: 'application/json' });
+      saveAs(blob, 'wallet.json');
+    }
+  };
+
+  const buttonSize = useBreakpointValue({ base: 'sm', md: 'md' }); // Adjust button size based on screen width
+
+
   return (
     <div className="App">
-
+      <VStack spacing={4} align="center">
+        <Text fontSize="xl">Crypto Wallet Generator</Text>
+        <Select
+          value={selectedBlockchain}
+          onChange={(e) => setSelectedBlockchain(e.target.value)}
+          width="80%"
+          maxWidth="300px"
+          variant="filled"
+        >
+          <option value="Ethereum">Ethereum</option>
+          <option value="Bitcoin">Bitcoin</option>
+          {/* Add more blockchain options here */}
+        </Select>
+        <Button
+          size={buttonSize}
+          colorScheme="blue"
+          onClick={generateWallet}
+          width="80%"
+          maxWidth="300px"
+        >
+          Generate Wallet
+        </Button>
+        {walletData && (
+          <Box width="80%" maxWidth="300px" borderWidth="1px" p={4} borderRadius="md">
+            <Text fontWeight="bold">Wallet Data</Text>
+            <Text>Public Key: {walletData.publicKey}</Text>
+            <Text>Secret Key Base58: {walletData.secretKeyBase58}</Text>
+            <Text>Secret Key Text: {walletData.secretKeyText}</Text>
+            <Text>Recovery Phrase: {walletData.recoveryPhrase}</Text>
+            <Button
+              size={buttonSize}
+              colorScheme="blue"
+              onClick={downloadAsJSON}
+              mt={4}
+              width="100%"
+            >
+              Download as JSON
+            </Button>
+          </Box>
+        )}
+      </VStack>
     </div >
   );
 }
